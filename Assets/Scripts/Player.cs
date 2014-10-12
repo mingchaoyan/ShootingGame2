e9ed8fbb1c2505282fd164464d354f761d160b88
mingchaoyan <mingchaoyan@gmail.com>
2014-10-12 21:40:48 +0800

    expandtab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	public float speed;
+    public float speed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float moveTo = Input.GetAxis("Horizontal") * speed;
-		gameObject.transform.Translate(Vector3.right * moveTo);
+    // Use this for initialization
+    void Start ()
+    {
 
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float moveTo = Input.GetAxis ("Horizontal") * speed;
+        gameObject.transform.Translate (Vector3.right * moveTo);
+
+
+    }
 }
