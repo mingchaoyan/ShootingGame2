5cb86e54d9cf6d055bd092065bd395114f923465
mingchaoyan <mingchaoyan@gmail.com>
2014-10-12 22:12:41 +0800

    实现主角循环移动

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,9 @@ public class Player : MonoBehaviour
         float moveTo = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
         gameObject.transform.Translate (Vector3.right * moveTo);
 
-
+        if(transform.position.x >= 7.8f)
+            transform.position = new Vector3(-7.8f, transform.position.y);
+        if(transform.position.x < -7.8f)
+            transform.position = new Vector3(7.8f, transform.position.y);
     }
 }
