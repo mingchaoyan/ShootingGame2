4025bd3dfdab52a65af8889c3dd59cd229443155
mingchaoyan <mingchaoyan@gmail.com>
2014-10-12 21:32:22 +0800

    添加一个二维Cube移动控制

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveTo = Input.GetAxis("Horizontal") * speed;
+		gameObject.transform.Translate(Vector3.right * moveTo);
+
+	
+	}
+}
