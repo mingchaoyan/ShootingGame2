5ace71265a1650138d9c6f41c16eee95ffef3349
mingchaoyan <mingchaoyan@gmail.com>
2014-10-13 22:50:59 +0800

    多个if 使用else 减少无用判断

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,28 +3,27 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+		public float speed;
+		public Transform projectile;
 
-	public float speed;
-	public Transform projectile;
+		// Use this for initialization
+		void Start ()
+		{
 
-	// Use this for initialization
-	void Start ()
-	{
+		}
 
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				float moveTo = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+				gameObject.transform.Translate (Vector3.right * moveTo);
 
-	// Update is called once per frame
-	void Update ()
-	{
-		float moveTo = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
-		gameObject.transform.Translate (Vector3.right * moveTo);
-
-		if (transform.position.x >= 7.8f)
-			transform.position = new Vector3 (-7.8f, transform.position.y);
-		if (transform.position.x < -7.8f)
-			transform.position = new Vector3 (7.8f, transform.position.y);
-		Vector3 position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y);
-		if (Input.GetKeyDown ("space"))
-			Instantiate (projectile, position, Quaternion.identity);
-	}
+				if (transform.position.x >= 7.8f)
+						transform.position = new Vector3 (-7.8f, transform.position.y);
+				else
+						transform.position = new Vector3 (7.8f, transform.position.y);
+				Vector3 position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+				if (Input.GetKeyDown ("space"))
+						Instantiate (projectile, position, Quaternion.identity);
+		}
 }
