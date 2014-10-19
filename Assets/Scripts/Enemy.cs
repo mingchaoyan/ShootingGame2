aa106363ca6487e01b849fc05788501c3096fe52
mingchaoyan <mingchaoyan@gmail.com>
2014-10-19 11:19:42 +0800

    创建敌人

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+		public float minSpeed;
+		public float maxSpeed;
+		private float currentSpeed;
+
+		float x, y, z;
+
+		// Use this for initialization
+		void Start ()
+		{
+				SetPostion ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				float toMove = currentSpeed * Time.deltaTime;
+				transform.Translate (Vector3.down * toMove);
+				if (transform.position.y < -4.5)
+						SetPostion ();
+	
+		}
+		void SetPostion ()
+		{
+				currentSpeed = Random.Range (minSpeed, maxSpeed);
+				x = Random.Range (-8.5f, 8.5f);
+				y = 6.5f;
+				transform.position = new Vector3 (x, y, z);
+		}
+}
