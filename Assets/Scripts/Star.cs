1c06ebd0f7a8b187d92607daccd0f08335dfe5cc
mingchaoyan <mingchaoyan@gmail.com>
2014-10-25 23:46:18 +0800

    星空背景　

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour
+{
+    public float starSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        float toMove = starSpeed * Time.deltaTime;
+        transform.Translate(Vector3.down * toMove, Space.World);
+
+        if (transform.position.y < -14f)
+            transform.position = new Vector3(transform.position.x, 19.5f, 10);
+    }
+}
