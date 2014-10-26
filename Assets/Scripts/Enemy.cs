f08bb7489121eed80fd7917fa18c607bedd07166
mingchaoyan <mingchaoyan@gmail.com>
2014-10-26 19:31:59 +0800

    敌人大小随机

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,12 @@ public class Enemy : MonoBehaviour
     public float maxSpeed;
     private float currentSpeed;
 
+    private float minScale = 0.5f;
+    private float maxScale = 1.2f;
+    private float curScaleX;
+    private float curScaleY;
+    private float curScaleZ;
+
     float x, y, z = 0;
 
     // Use this for initialization
@@ -30,6 +36,10 @@ public class Enemy : MonoBehaviour
     public void InitPositionAndSpeed()
     {
         currentSpeed = Random.Range(minSpeed, maxSpeed);
+        curScaleX = Random.Range(minScale, maxScale);
+        curScaleY = Random.Range(minScale, maxScale);
+        curScaleZ = Random.Range(minScale, maxScale);
+        transform.localScale = new Vector3(curScaleX, curScaleY, curScaleZ);
         x = Random.Range(-8.5f, 8.5f);
         y = 6.5f;
         transform.position = new Vector3(x, y, z);
